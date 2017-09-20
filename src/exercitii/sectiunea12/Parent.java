439e2d38c6e0fe8dd2bff6092e0a1216477e70d2
Ioana <ioanaimbuzean@ymail.com>
2017-09-20 20:39:02 +0300

    initial commit

---
+++ src/exercitii/sectiunea12/Parent.java
@@ -0,0 +1,19 @@
+package exercitii.sectiunea12;
+
+import java.io.IOException;
+
+/**
+ * Created by Ioana on 2017-09-06.
+ */
+class Parent {
+        static void say() throws IOException {
+        }
+
+    public static void main(String[] args) {
+        String st = "Hello";
+    }
+    }
+
+
+
+
